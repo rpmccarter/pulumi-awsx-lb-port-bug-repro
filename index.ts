32cf1318cec38062f31c5061f28174a61a07ecb8
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-05-24 18:27:34 -0700

    simplify even further

--- index.ts
+++ index.ts
@@ -1,27 +1,17 @@
-import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
-import * as pulumi from '@pulumi/pulumi';
 
-const stack = pulumi.getStack();
-
-const defaultVpc = new awsx.ec2.DefaultVpc('default-vpc');
-
-const serverTg = pulumi.Output.create(new aws.lb.TargetGroup(`test-server-tg-${stack}`, {
-  vpcId: defaultVpc.vpcId,
-  port: 80,
-  protocol: 'HTTP',
-  targetType: 'ip',
-  protocolVersion: 'HTTP1',
-}));
-
-new awsx.lb.ApplicationLoadBalancer(`test-lb-${stack}`, {
+new awsx.lb.ApplicationLoadBalancer(`test-lb`, {
   listeners: [
     {
       port: 443,
       protocol: 'HTTP',
       defaultActions: [{
-        type: 'forward',
-        targetGroupArn: serverTg.arn,
+        type: "fixed-response",
+        fixedResponse: {
+          contentType: "text/plain",
+          messageBody: "Hello world",
+          statusCode: "200",
+        },
       }],
     }
   ]
